f2384cb9c158842d7c2b4a043705e5cb0b72b3a3
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-01-05 04:37:55 -0800

    Fixed betty in 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,14 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm.
+ * @array: array of numbers to be ordened
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
@@ -11,15 +20,15 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (array[j] < array[j -1])
+			if (array[j] < array[j - 1])
 			{
 				aux = array[j];
 				array[j] = array[j - 1];
-				array[j -1] = aux;
+				array[j - 1] = aux;
 				print_array(array, size);
 				count++;
-	
-	
+
+
 			}
 		}
 	}

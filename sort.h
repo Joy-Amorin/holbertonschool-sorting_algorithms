c527dcab2cce8fcb7b356d9999f1acf3cd9fcd44
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-01-05 04:46:53 -0800

    fixed betty on sort.h

--- sort.h
+++ sort.h
@@ -1,4 +1,4 @@
-#ifndef SOTR_H
+#ifndef SORT_H
 #define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);

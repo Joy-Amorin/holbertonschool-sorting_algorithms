1e2af78e82d9b2470949db1bc86db62cb8095c42
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-07 10:09:52 -0800

    tsk 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts doubly linked list of integers in ascending order by insertion 
+ *
+ *@list: list_t node.
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node, *tmp, *aux;
+
+	if (list && *list == NULL)
+		return;
+	node = *list;
+	while ((node = node->next))
+	{
+		tmp = node;
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			aux = tmp->prev;
+			if (tmp->next)
+				tmp->next->prev = aux;
+			if (aux->prev)
+				aux->prev->next = tmp;
+			else
+				*list = tmp;
+			aux->next = tmp->next;
+			tmp->prev = aux->prev;
+			tmp->next = aux;
+			aux->prev = tmp;
+			print_list(*list);
+		}
+	}
+}
